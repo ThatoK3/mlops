83b5129afc6c64092a63cbceba48ed0398d9fbb1
Thato Kekana <thatojoe@outlook.com>
2025-06-09 22:18:07 +0000

    initial experiments

--- mlflow_server.sh
+++ mlflow_server.sh
@@ -1,10 +1,10 @@
 docker run -d \
   --name mlflow-server \
   --network="host" \
-  -v $(pwd)/mlruns:/mlflow/mlruns \
+  -v $(pwd)/notebook_experiments/mlruns:/mlflow/mlruns \
   mlflow-mysql \
   mlflow server \
     --backend-store-uri mysql+pymysql://mlflow:mlflowpass@45.151.153.107:3306/mlflow_db \
-    --default-artifact-root /mlflow/mlruns \
+    --default-artifact-root file:/mlflow/mlruns \
     --host 0.0.0.0 \
     --port 5000
