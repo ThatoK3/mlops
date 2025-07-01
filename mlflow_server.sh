e024225170813c8ecaf00c151101c93338d38513
Thato Joe <ThatoJoe@outlook.com>
2025-07-01 18:16:57 +0000

    with dummy .env

--- mlflow_server.sh
+++ mlflow_server.sh
@@ -1,12 +1,20 @@
+#!/bin/bash
+
+set -a
+source .env
+set +a
+
 docker stop mlflow-server
 docker rm mlflow-server
+
 docker run -d \
   --name mlflow-server \
   --network="host" \
   -v $(pwd)/notebook_experiments/mlruns:/mlflow/mlruns \
   thatojoe/mlflow-mysql \
   mlflow server \
-    --backend-store-uri mysql+pymysql://mlflow:mlflowpass@103.54.58.78:3306/mlflow_db \
+    --backend-store-uri mysql+pymysql://$MYSQL_USER:$MYSQL_PASSWORD@$MYSQL_HOST:$MYSQL_PORT/$MYSQL_DB \
     --default-artifact-root file:/mlflow/mlruns \
     --host 0.0.0.0 \
     --port 5000
+
