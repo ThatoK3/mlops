1748cd061132b56b4d0a6f0c78cd43113c852387
Thato Joe <ThatJoe@outlook.com>
2025-07-05 05:04:54 +0000

    mysql backup

--- mlflow_server_file_based.sh
+++
@@ -1,6 +0,0 @@
-docker exec -d mlflow-server \
-  mlflow server \
-    --backend-store-uri file:/mlflow/mlruns \
-    --default-artifact-root file:/mlflow/mlruns \
-    --host 0.0.0.0 \
-    --port 5001
